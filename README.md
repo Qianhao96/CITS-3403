5de5c1e803469515b77b98c7cfe64491048dfa84
Qianhao96 <qianhao.liu@gmail.com>
2019-02-24 00:25:27 +0800

    user authentication

--- README.md
+++ README.md
@@ -34,6 +34,9 @@ the purpose of the web application, explaining both the context and the social c
   - pip3 install flask 
   - pip3 install flask-sqlalchemy
   - pip3 install flask-wtf
+  - pip3 install flask-bcrypt
+  - pip3 install flask-login (used for session)
+  - python3 run.py
   - In Python3 shell 
     - from survey import db
     - db.create_all()
--- survey/forms.py
+++ survey/forms.py
@@ -1,21 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, RadioField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from survey.models import User
 
 
 class RegistrationForm(FlaskForm):
 	firstname = StringField('Firstname',
 		validators=[DataRequired(), Length(min=2, max=20)])
-	lastname = StringField('lastname',
+	lastname = StringField('Lastname',
 		validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email', 
 		validators=[DataRequired(), Email()])
-	gender = RadioField('Gender', choices = [('M','Male'),('F','Female')], validators=[DataRequired()])
+	gender = RadioField('Gender', choices = [('M','Male'),('F','Female')])
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password', 
 		validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign up ')
 
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('That email is taken. Please choose a different one')
+
+	def validate_gender(self, gender):
+		if gender.data == 'None':
+			raise ValidationError('Please secect your gender')
 
 
 class LoginForm(FlaskForm):
