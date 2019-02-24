643ff1d405e84f66b0531203604f0f9149c0fb64
Qianhao96 <qianhao.liu@gmail.com>
2019-02-24 22:00:37 +0800

    user reset password using email

--- survey/models.py
+++ survey/models.py
@@ -1,4 +1,5 @@
-from survey import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from survey import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -13,6 +14,22 @@ class User(db.Model, UserMixin):
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	gender = db.Column(db.String(5), nullable=False)
 	password = db.Column(db.String(60), nullable=False)
+
+
+	def get_reset_token(self, expires_sec=1800):
+		s = Serializer(app.config['SECRET_KEY'], expires_sec)
+		return s.dumps({'user_id': self.id}).decode('utf-8')
+
+
+	@staticmethod
+	def verify_reset_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
+		try:
+			user_id = s.loads(token)['user_id']
+		except:
+			return None
+		return User.query.get(user_id)
+
 
 	def __repr__(self):
 		return f"User('{self.firstname}', '{self.lastname}', '{self.email}')"
