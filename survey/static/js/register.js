72330549d22aa873deff1cf02df9b8452acde38b
Cunjun Yin <toomato@Cunjuns-MacBook-Pro.local>
2019-02-25 14:32:01 +0800

    Add color

--- survey/static/js/register.js
+++ survey/static/js/register.js
@@ -12,7 +12,7 @@ var prama = {
     pass: true
 };
 
-var displayed = false;
+var display = false;
 
 // red orange green blue
 function passwordC() {
@@ -31,40 +31,55 @@ function passwordC() {
         }
     }
 
-    var score;
+    var score = 50;
 
-    if (PWDlength < 8){
-        score = 50;
-    }else{
-        score = 100;
+    if (PWDlength < 7) {
+        if (!display) {
+            $("#strength").html("Add " + (7 - PWDlength).toString() + " characters");
+        }
+        display = true;
     }
 
-    if (PWDlength > 8 && PWDlength <= 13) {
-        score +=(13-PWDlength)*10;
+    if (PWDlength > 8 && PWDlength < 13) {
+        score += 50 - ((13 - PWDlength) * 10);
     }
 
-    if (score > 100) {
+    if (score > 100 || PWDlength >= 13) {
         score = 100;
     }
 
     // password has at least 1 sybols
-    var symbols = '.*[!,@,#,$,%,^,&,*,?,_,~]';
-    symbols = new RegExp('(' + symbols + ')');
-    if (!password.match(symbols)) {
+    if (!password.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/)) {
+        if (!display) {
+            $("#strength").html("Add Some Symbols to make it stronger");
+        }
+        display = true;
         score -= 10;
     }
 
     // password has Upper and Lower chars
     if (!password.match(/([a-z])/)) {
+        if (!display) {
+            $("#strength").html("Add some Lower case characters to make it stronger");
+        }
+        display = true;
         score -= 10;
     }
 
     if (!password.match(/([A-Z])/)) {
+        if (!display) {
+            $("#strength").html("Add some Upper case characters to make it stronger");
+        }
+         display = true;
         score -= 15;
     }
 
     // password has number
     if (!password.match(/([0-9])/)) {
+        if (!display) {
+            $("#strength").html("Add Some Numbers to make it stronger");
+        }
+        display = true;
         score -= 15;
     }
 
@@ -72,13 +87,24 @@ function passwordC() {
         score = 0;
     }
 
+    if (score >= 25 && score <= 50) {
+        $("#strength").css("background-color", "orange");
+    }else if(score > 50 && score <= 75){
+        $("#strength").css("background-color", "green");
+    }else if (score > 75){
+        $("#strength").css("background-color", "blue");
+    }else{
+        $("#strength").css("background-color", "red");
+    }
+
     var percent = score.toString() + "%";
     $("#strength").css("width", percent);
+    display = false;
 }
 
 jQuery((document)).ready(function () {
     "use strict";
-    $("#password").keydown(function () {
+    $("#password").keypress(function () {
         passwordC();
     });
 });
