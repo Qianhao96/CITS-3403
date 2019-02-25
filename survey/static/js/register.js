8b5d4462b8e9dec2c38bb243cdaaf3d67179d474
Cunjun Yin <toomato@Cunjuns-MacBook-Pro.local>
2019-02-25 13:36:45 +0800

    Move able progresss

--- survey/static/js/register.js
+++ survey/static/js/register.js
@@ -1,7 +1,7 @@
 var prama = {
     containsUsername: "ops, seems like this passwork contain the username",
     shortPWD: "The password is too short",
-    weekPWD: "Toooo week; combination of letters & numbers, plz",
+    weekPWD: "Too week; combination of letters & numbers, plz",
     mediumPWD: "OkOk; try some symbols to make it harder",
     strongPWD: "Strong Passwork",
     onlyChar: "add some Numbers & Symbols",
@@ -12,63 +12,74 @@ var prama = {
     pass: true
 };
 
-function strength(strength) {
-    switch (strength) {
-        // contain user name
-        case 0:
-            break;
-            // too short
-        case 10:
-            break;
-            // only char
-        case 20:
-            break;
-            // only number
-        case 30:
-            break;
-            // only symbols
-        case 40:
-            break;
-            //1 uper case, char, number
-        case 50:
-            return 0;
-    }
-}
+var displayed = false;
 
-function password(PWD, username) {
+// red orange green blue
+function passwordC() {
+    "use strict";
+    var username = $('#firstname').val() + $('#lastname').val();
+    var password = $("#password").val();
+    var PWDlength = password.length;
     // password === username
-    if (PWD.toLowerCase().equals(username.toLowerCase())) {
-        return 0;
-    }
-    var user = new RegExp(username.toLowerCase());
-    if (password.toLowerCase().match(user)) {
-        return 0;
-    }
-
-    //test password requirement
-    PWDlength = PWD.length;
-    if (PWDlength < 8) return 10;
-
-    if (PWD.match(/^\w+$/)) {
-        return 20;
-    }
-    if (PWD.match(/^\d+$/)) {
-        return 30;
-    }
-    if (PWD.match()) {
-        return 40;
+    if (username) {
+        if (password.toLowerCase().equals(username.toLowerCase())) {
+            return 0;
+        }
+        var user = new RegExp(username.toLowerCase());
+        if (password.toLowerCase().match(user)) {
+            return 0;
+        }
     }
 
-    // pass test initialize score to 0
-    score = 0;
-    //normalization
-    if (PWD.match(/([a-z])/) && PWD.match(/([0-9])/) && PWD.match(/([A-Z])/)) {
-        if(PWDlength>13) PWDlength=13;
-        score = (100-(13-PWDlength))*10;
+    var score = 100;
+    if (PWDlength < 8) {
+        score = 50;
     }
-    return score;
+
+    if (PWDlength > 8 && PWDlength <= 13) {
+        score -= (13 - PWDlength) * 10;
+    }
+    // password has 3 numbers
+    if (password.match(/(.*[0-9].*[0-9].*[0-9])/)) {
+        score += 5;
+    }
+
+    // password has at least 2 sybols
+    var symbols = '.*[!,@,#,$,%,^,&,*,?,_,~]';
+    symbols = new RegExp('(' + symbols + symbols + ')');
+    if (password.match(symbols)) {
+        score += 5;
+    }
+
+    // password has Upper and Lower chars
+    if (!password.match(/([a-z])/)) {
+        score -= 10;
+    }
+
+    if (!password.match(/([A-Z])/)) {
+        score -= 10;
+    }
+
+    // password has number and chars
+    if (!password.match(/([0-9])/)) {
+        score -= 15;
+    }
+
+    if (score > 100) {
+        score = 100;
+    }
+
+    if (score < 0 || PWDlength === 0) {
+        score = 0;
+    }
+
+    var percent = score.toString() + "%";
+    $("#strength").css("width", percent);
 }
 
-function init(){
-
-}
+jQuery((document)).ready(function () {
+    "use strict";
+    $("#password").keydown(function () {
+        passwordC();
+    });
+});
