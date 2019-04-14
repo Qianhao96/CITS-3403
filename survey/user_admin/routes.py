ddfc4852023113bcad60f71944d8beecee0cdfe9
Qianhao96 <qianhao.liu@gmail.com>
2019-04-14 23:52:42 +0800

    Add user-admin blueprint

--- survey/user_admin/routes.py
+++ survey/user_admin/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, Blueprint
+from survey.models import User
 
-user_admin = Blueprint('user-admin', __name__)
+user_admin = Blueprint('user_admin', __name__)
 
 @user_admin.route("/user_admin")
 def index():
-	return render_template('user_admin.html')
+	users = User.query.all()
+	return render_template('user_admin.html', users=users, title="Admin")
 
