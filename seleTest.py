d59f54ba6329309036c986e76f5b5a6a996317c4
Qianhao96 <qianhao.liu@gmail.com>
2019-05-16 22:08:22 +0800

    adding category and polls

--- seleTest.py
+++ seleTest.py
@@ -163,6 +163,43 @@ class LoginLogoutRegisterTest(unittest.TestCase):
 		self.driver.close()
 
 
+class LoginLogoutRegisterTest(unittest.TestCase):
+	def setUp(self):
+		executable_path='/usr/local/bin/chromedriver'
+		self.driver = webdriver.Chrome(executable_path)
+
+	def test_f_create_user_and_vote(self):		
+		driver = self.driver
+		# driver.get(domain + "/login")
+		for i in range(1,21):
+			driver.get("http://0.0.0.0:5000/register")
+			self.assertIn("Register", driver.title)
+			firstname=driver.find_element_by_id("firstname")
+			firstname.clear()
+			firstname.send_keys("test")
+			lastname=driver.find_element_by_id("lastname")
+			lastname.clear()
+			lastname.send_keys("test")
+			email=driver.find_element_by_id("email")
+			email.clear()
+			email.send_keys("test" +str(i)+ "@gmail.com")
+			gender=driver.find_element_by_id("gender-0")
+			gender.send_keys(Keys.SPACE)
+			password=driver.find_element_by_id("password")
+			password.clear()
+			password.send_keys("test12345678")
+			confirm_password=driver.find_element_by_id("confirm_password")
+			confirm_password.clear()
+			confirm_password.send_keys("test12345678")
+			time.sleep(1)
+			driver.find_element_by_id("submit").send_keys(Keys.ENTER)
+			time.sleep(1)
+			assert "Your account has been created! You are now able to login" in driver.page_source
+			driver.get("http://localhost:5000/polls")
+
+	def tearDown(self):
+		self.driver.close()
+
 if __name__ == "__main__":
     unittest.main()
 
