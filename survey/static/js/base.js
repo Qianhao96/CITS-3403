5a5c29135efbc74ad5009fec7ea2451af65bc39b
CunjunYin <cunjunyin@outlook.com>
2019-05-16 19:13:58 +0800

    last one forgot to add

--- survey/static/js/base.js
+++ survey/static/js/base.js
@@ -1,13 +1,23 @@
+var dw_id = ["about", "Deve", "ur-info"]
+
 $(document).ready(function () {
-	setInterval(function() {
-  		dateTime()
+	setInterval(function () {
+		dateTime()
 	}, 500);
+
+	$(".click-dropsown").click(function () {
+		var ids = $(this).attr("id");
+		for (var i = 0; i < 3; i++) {
+			if (dw_id[i] != ids) $("." + dw_id[i]).hide();
+		}
+		$("." + ids).show("slow");
+	});
 });
 
-function dateTime(){
+function dateTime() {
 	var today = new Date();
-	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-	var dateTime = date+' '+time;
+	var dateTime = date + ' ' + time;
 	$('#current_time').html(dateTime);
 }
