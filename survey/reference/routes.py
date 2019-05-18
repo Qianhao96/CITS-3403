fc3c95ab96ba94d44c1d0dd1dce3c4845b9b2a08
CunjunYin <cunjunyin@outlook.com>
2019-05-18 12:40:34 +0800

    reference

--- survey/reference/routes.py
+++ survey/reference/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from survey.main.routes import get_client
+
 
 references = Blueprint('references', __name__)
 
 @references.route("/reference", methods=['GET'])
 def reference():
-	return render_template('reference/reference.html')
+	return render_template('reference/reference.html', client= get_client())
